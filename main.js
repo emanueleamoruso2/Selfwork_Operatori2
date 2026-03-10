cfbc7475ed75e286b1ed17a3620f8349dc823198
emanueleamoruso2 <emanuele.amoruso.a@gmail.com>
2026-03-10 13:23:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-let dateobj = new Date();
-let current_year = dateobj.getFullYear();
-console.log(current_year);
-let anno_nascita = Number(prompt('Il tuo anno di nascita?'));
+let current_year = 2026;
+let anno_nascita = 2000;
 let eta_persona= current_year - anno_nascita;
 let years_necessary_to_100= 100 - eta_persona;
+
 console.log(years_necessary_to_100);
