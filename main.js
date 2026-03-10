e0b96b067c8fd13c51ac783b3af65b78c7c6d706
emanueleamoruso2 <emanuele.amoruso.a@gmail.com>
2026-03-10 16:56:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ let anno_nascita = 2000;
 let eta_persona= current_year - anno_nascita;
 let years_necessary_to_100= 100 - eta_persona;
 
-console.log(years_necessary_to_100);
+console.log(`Hai ${eta_persona} anni e ti mancano ${year_necessary_to_100} per compierne 100`)
